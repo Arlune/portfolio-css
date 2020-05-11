0e3b33eb3fa87473b715137d62bf4d065b57a8b0
Arlune <morgane.gaillard69@gmail.com>
2020-05-11 12:05:06 +0200

    add docs

---
+++ docs/precache-manifest.faa6d95e1294cf18aa313386b72992ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb51c2ac677b8c755a85f160fc5cd5d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80aa6a72c7c89e2ac8d0",
+    "url": "/static/css/main.e47e03b5.chunk.css"
+  },
+  {
+    "revision": "9525efdb37d4685087db",
+    "url": "/static/js/2.49465c16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.49465c16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80aa6a72c7c89e2ac8d0",
+    "url": "/static/js/main.e4697f99.chunk.js"
+  },
+  {
+    "revision": "af4f3b60a09dd36a8c54",
+    "url": "/static/js/runtime-main.0e80d412.js"
+  }
+]);
